3dac59a5184624c030e393e3b86ab308e56fad01
3Asterism <842384412@qq.com>
2023-01-13 11:44:46 +0800

    springboot框架第一次上传

---
+++ src/main/java/com/akisan/uploadProjectData/uploadProjectDataApplication.java
@@ -0,0 +1,11 @@
+package com.akisan.uploadProjectData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class uploadProjectDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(uploadProjectDataApplication.class, args);
+    }
+}
